5aa9f432bfd23553b9f1370966478fc0b52b0395
Anton Khomenko <regiysa@gmail.com>
2023-06-18 23:44:50 +0300

    style: fix aside height

--- admin/src/components/aside.component.tsx
+++ admin/src/components/aside.component.tsx
@@ -162,7 +162,6 @@ const Sidebar: React.FC = () => {
 };
 
 const SidebarContainer = styled.aside`
-  height: calc(100vh - 50px);
   background-color: rgb(251, 251, 255);
   position: sticky;
   top: 50px;
