edd56645cc22b355133b05ddab2c891199e83cd3
Anton Khomenko <regiysa@gmail.com>
2023-06-28 17:31:06 +0300

    fix: api rollback

--- api/models/product.ts
+++ api/models/product.ts
@@ -18,7 +18,7 @@ const ProductsSchema = new mongoose.Schema({
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
-    altImg: { type: String },
+    altImg: { type: String, required: true },
     categories: { type: [String] },
     size: { type: [String], required: true },
     color: { type: [String], required: true },
