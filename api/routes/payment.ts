64f43d7e6243e5d095edb7944ae93d3f854371a0
Anton Khomenko <regiysa@gmail.com>
2023-07-10 19:19:37 +0300

    fix: debug get-order liqpay

--- api/routes/payment.ts
+++ api/routes/payment.ts
@@ -1,14 +1,13 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import axios from "axios";
+import qs from "querystring";
 require('dotenv').config();
-const router = require("express").Router()
+const router = require("express").Router();
 const LiqPay = require('./liqpay');
-
 
 const liqpay = new LiqPay(process.env.LIQPAY_PUBLIC_KEY, process.env.LIQPAY_PRIVATE_KEY);
 
-
 router.post("/", async (req: Request, res: Response) => {
-
     const order = liqpay.cnb_form({
         'action': 'pay',
         'amount': req.body.amount,
@@ -19,24 +18,35 @@ router.post("/", async (req: Request, res: Response) => {
         'result_url': 'https://mern-shop-client.vercel.app',
         'server_url': 'https://mern-shop-api.vercel.app'
     });
-    res.send(order)
-})
-
+    res.send(order);
+});
 
 router.get("/get-order/:order_id", async (req, res) => {
     const orderId = req.params.order_id;
 
-    liqpay.api("request", {
-        "action": "status",
-        "version": "3",
-        "order_id": orderId
-    }, function(json) {
-        // Обработка ответа и отправка данных о товаре
-        const orderStatus = json.status;
-        res.status(200).json({ status: orderStatus });
-    });
+    const data = {
+        action: 'status',
+        version: '3',
+        order_id: orderId,
+    };
+
+    const dataString = qs.stringify(data);
+    const signature = Buffer.from(process.env.LIQPAY_PRIVATE_KEY + dataString + process.env.LIQPAY_PRIVATE_KEY).toString('base64');
+
+    const requestData = {
+        data: dataString,
+        signature: signature,
+    };
+
+    axios.post('https://www.liqpay.ua/api/request', qs.stringify(requestData))
+        .then(response => {
+            const orderStatus = response.data.status;
+            res.status(200).json({ status: orderStatus });
+        })
+        .catch(error => {
+            console.error(error);
+            res.sendStatus(500);
+        });
 });
-
-
 
 module.exports = router;
