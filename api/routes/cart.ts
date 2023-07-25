cc7cd7251cca61a97f47310bf03be9d8d8c64164
Anton Khomenko <regiysa@gmail.com>
2023-07-25 19:35:43 +0300

    fix: cart websocket

--- api/routes/cart.ts
+++ api/routes/cart.ts
@@ -17,12 +17,16 @@ router.post("/", verifyToken, async (req: any, res: Response) => {
     try {
         const userId = req.user.id;
         const newCart = req.body.products;
-        const options: { new: boolean; upsert: true } = { new: true, upsert: true };
-        const updatedCart = await Cart.findOneAndUpdate({ userId }, { products: newCart }, options);
-        const io = req.app.get("socketServer").io;
+        const updatedCart = await Cart.findOneAndUpdate(
+            { userId },
+            { products: newCart },
+            { new: true, upsert: true }
+        );
 
-        io.emit(`cartUpdated:${userId}`, updatedCart);
-        console.log(`Emitted cartUpdated event for user ${userId}`);
+        // const io = req.app.get("socketServer").io;
+
+        // io.emit(`cartUpdated:${userId}`, updatedCart);
+        // console.log(`Emitted cartUpdated event for user ${userId}`);
         res.status(200).json(updatedCart);
     } catch (error) {
         console.error("Error updating cart:", error);
